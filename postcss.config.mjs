4fb7749ba7471826676865499eada990ba449fc6
jayzone91 <register@j4yz0n3.de>
2025-02-19 12:23:08 +0100

    new t3

--- postcss.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-    "postcss-hover-media-feature": {},
-  },
-};
-
-export default config;
